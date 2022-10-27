0f8a7a973e5763496a6d487da05b91ede370472c
Jie Li <JLi147@slb.com>
2022-10-27 11:28:09 +0800

    feat: add a car can show message when speed up

---
+++ OOPractice/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPractice
+{
+    public class Car
+    {
+        private string name;
+        private int speed;
+
+        public Car(string name, int speed)
+        {
+            this.name = name;
+            this.speed = speed;
+        }
+
+        public string SpeedUp()
+        {
+            return this.name + ": speed up " + this.speed + " km/h";
+        }
+    }
+}
---
+++ OOPracticeTest/CarTest.cs
@@ -0,0 +1,24 @@
+﻿using OOPractice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OOPracticeTest
+{
+    public class CarTest
+    {
+        [Fact]
+        public void Should_return_message_when_speed_up_given_car_have_name_speed()
+        {
+            // given
+            Car car = new Car("Cool Car", 30);
+            // when
+            string message = car.SpeedUp();
+            // then
+            Assert.Equal("Cool Car: speed up 30 km/h", message);
+        }
+    }
+}
