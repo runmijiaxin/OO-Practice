ea9e5a73825cbf17f09deb8ee00bafc8aba2daed
Jie Li <JLi147@slb.com>
2022-10-27 11:41:54 +0800

    feat: add a truck when speed up return message

---
+++ OOPractice/Truck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPractice
+{
+    public class Truck
+    {
+        private string name;
+        private int speed;
+
+        public Truck(string name, int speed)
+        {
+            this.name = name;
+            this.speed = speed;
+        }
+
+        public string SpeedUp()
+        {
+            return $"{this.name}: speed up {this.speed} km/h";
+        }
+    }
+}
---
+++ OOPracticeTest/TruckTest.cs
@@ -0,0 +1,24 @@
+﻿using OOPractice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OOPracticeTest
+{
+    public class TruckTest
+    {
+        [Fact]
+        public void Should_return_message_when_speed_up_given_truck_have_name_speed()
+        {
+            // given
+            Truck truck = new Truck("Big Truck", 10);
+            // when
+            string message = truck.SpeedUp();
+            // then
+            Assert.Equal("Big Truck: speed up 10 km/h", message);
+        }
+    }
+}
